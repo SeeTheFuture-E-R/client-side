a407f2e73590ace40f2c74c797dbc1af67f009fe
esterzinger <esterrzinger@gmail.com>
2023-11-06 12:53:25 +0200

    uploud changes in nav

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css"
+function Footer(){
+    return(
+<div id="Footer">
+    <br/><br/>
+       0504168964    קריית יערים         אנחנו כאן בשבילך                 ezer_ester_renana@gmail.com <br/><br/>
+   0583236977    בית וגן ירושלים      נשמח להיות בקשר         לפניות, הערות והארות חייגו 1-700-661-305
+   <br/><br/>
+   </div>
+    )
+}
+export default Footer;
+//
